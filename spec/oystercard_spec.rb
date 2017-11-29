e3ab0c9dd90b93e85a71ccd0de16489404591aac
Eleanor Wintram <eleanorwintram@gmail.com>
2017-11-29 15:10:26 +0000

    refactoring

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,46 +2,44 @@ require 'oystercard'
 
 describe Oystercard do
 
-  it { is_expected.to respond_to :balance }
-
+  subject(:oystercard) { described_class.new }
   let(:station) { double(:station) }
+
+  before do |example|
+    unless example.metadata[:skip_before]
+      oystercard.top_up(Oystercard::DEFAULT_MINIMUM)
+      oystercard.touch_in(station)
+    end
+  end
 
   describe "#initialize" do
 
-    it "initializes with a balance of zero" do
-      expect(subject.balance).to eq 0
+    it "initializes with a balance of zero", :skip_before do
+      expect(oystercard.balance).to be_zero
     end
 
     it "initializes with an empty array of journeys" do
-      expect(subject.journeys).to eq []
+      expect(oystercard.journeys).to eq []
     end
 
   end
 
   describe "#top_up" do
 
-    it { is_expected.to respond_to(:top_up).with(1).argument }
-
     it "adds a given amount to the card" do
-      expect{ subject.top_up(10) }.to change {subject.balance}.by 10
+      expect{ oystercard.top_up(10) }.to change {oystercard.balance}.by 10
     end
 
-    it "sets a limit to the deafult limit" do
-      limit = Oystercard::DEFAULT_LIMIT
-      subject.top_up(limit)
-      expect{ subject.top_up(1) }.to raise_error "You have reached a top up limit of £#{limit}"
+    it "raises an error if the default limit is reached", :skip_before do
+      oystercard.top_up(Oystercard::DEFAULT_LIMIT)
+      expect{ oystercard.top_up(1) }.to raise_error "You have reached a top up limit of £#{Oystercard::DEFAULT_LIMIT}"
     end
 
-    it "raises error if initial balance is not nil before top_up" do
-      limit = Oystercard::DEFAULT_LIMIT
-      subject.top_up(10)
-      expect{ subject.top_up(limit) }.to raise_error "You have reached a top up limit of £#{limit}"
-    end
   end
 
   describe "#in_journey?" do
-    it "checks if it is initially not in journey" do
-      expect(subject).not_to be_in_journey
+    it "checks if it is in journey" do
+      expect(oystercard).to be_in_journey
     end
   end
 
@@ -49,22 +47,17 @@ describe Oystercard do
 
       context "with top-up" do
 
-        before do
-          subject.top_up(Oystercard::DEFAULT_MINIMUM)
-          subject.touch_in(station)
-        end
-
         it "should be in_journey when touched in" do
-          expect(subject).to be_in_journey
+          expect(oystercard).to be_in_journey
         end
 
         it "stores the start station in instance variable" do
-          expect(subject.entry_station).to eq(station)
+          expect(oystercard.entry_station).to eq(station)
         end
       end
 
-    it "raises an error when attempting to touch in with balance of < £#{Oystercard::DEFAULT_MINIMUM}" do
-      expect {subject.touch_in(station) }.to raise_error "Insufficient balance for journey"
+    it "raises an error when attempting to touch in with balance of < £#{Oystercard::DEFAULT_MINIMUM}", :skip_before do
+      expect {oystercard.touch_in(station) }.to raise_error "Insufficient balance for journey"
     end
 
 
@@ -73,40 +66,40 @@ describe Oystercard do
   describe "#touch_out" do
 
     before do
-      subject.top_up(10)
-      subject.touch_in(station)
-      subject.touch_out(station)
+      oystercard.top_up(10)
+      oystercard.touch_in(station)
+      oystercard.touch_out(station)
     end
      it "should not be in_journey when touched out" do
-       expect(subject).to_not be_in_journey
+       expect(oystercard).to_not be_in_journey
      end
 
      it "deducts a given amount when touched out" do
-       expect { subject.touch_out(station) }.to change{ subject.balance }.by(-Oystercard::DEFAULT_MINIMUM)
+       expect { oystercard.touch_out(station) }.to change{ oystercard.balance }.by(-Oystercard::DEFAULT_MINIMUM)
      end
 
      it "removes the entry station from the instance variable" do
-       expect(subject.entry_station).to eq nil
+       expect(oystercard.entry_station).to eq nil
      end
 
      it "stores the exit station in an instance variable" do
-       expect(subject.exit_station).to eq(station)
+       expect(oystercard.exit_station).to eq(station)
      end
   end
 
   describe "#record_journey" do
 
     before do
-      subject.top_up(10)
-      subject.touch_in(station)
-      subject.touch_out(station)
+      oystercard.top_up(10)
+      oystercard.touch_in(station)
+      oystercard.touch_out(station)
     end
     it "adds the exit station to a journey hash" do
-      expect(subject.journey).to include(:entry_station => station, :exit_station => station)
+      expect(oystercard.journey).to include(:entry_station => station, :exit_station => station)
     end
 
     it "pushes the journey has into the journeys array" do
-      expect(subject.journeys).to include(subject.journey)
+      expect(oystercard.journeys).to include(oystercard.journey)
     end
 
   end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,16 +2,16 @@ require 'station'
 
 describe Station do
 
-  subject {described_class.new("Bank", 1)}
+  subject(:station) {described_class.new("Bank", 1)}
 
   describe "#initialize" do
 
     it "initializes with a name" do
-      expect(subject.station_name).to eq("Bank")
+      expect(station.station_name).to eq("Bank")
     end
 
     it "initializes with a zone" do
-      expect(subject.station_zone).to eq 1
+      expect(station.station_zone).to eq 1
     end
   end
 
